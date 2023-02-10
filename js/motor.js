808958fd261cf2ff73d94b8d593f84c7e8550a27
neyserht <neyser.nht@gmail.com>
2023-02-10 13:19:09 +0100

    prueba de boton a imagen

--- js/motor.js
+++ js/motor.js
@@ -1,4 +1,4 @@
-function peticionServidor1(d1)
+function peticionServidor1(servidor1, d1)
 {
     let ajax1 = new XMLHttpRequest();
 
@@ -11,12 +11,25 @@ function peticionServidor1(d1)
     };
 
     // 2. Envío de la petición al servidor
-    ajax1.open('GET','fichero1.html', true);
+    ajax1.open('GET',servidor1, true);
     ajax1.send();
 }
 
-function peticionServidor2(d1)
+function peticionServidor2(servidor1, d1)
 {
+    let ajax1 = new XMLHttpRequest();
+
+    // 1. Devolución del servidor
+    ajax1.onreadystatechange = function(){
+        if (ajax1.readyState==4 && ajax1.status==200)
+        {
+            document.getElementById(d1.id).innerHTML = ajax1.responseText;
+        }
+    };
+
+    // 2. Envío de la petición al servidor
+    ajax1.open('GET',servidor1, true);
+    ajax1.send();
 }
 
 // Cuando Todos los recursos se hayan cargado...
@@ -25,13 +38,23 @@ window.addEventListener("load", function(event){
     const b1 = document.getElementById("b1");
     const b2 = document.getElementById("b2");
     const d1 = document.getElementById("d1");
+    let servidor1 = "";
 
     // Asociar el elementoal evento y llamada a la función
     if(b1)
     {
         b1.addEventListener("click", function(event){
             event.preventDefault();
-            peticionServidor1(d1);
+            servidor1 = "fichero1.html";
+            peticionServidor1(servidor1, d1);
+        });
+    }
+    if(b2)
+    {
+        b2.addEventListener("click", function(event){
+            event.preventDefault();
+            servidor1 = "cargar-imagen1.html";
+            peticionServidor2(servidor1, d1);
         });
     }
 });
